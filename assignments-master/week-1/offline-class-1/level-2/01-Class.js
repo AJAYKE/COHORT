90bf73f2a04b335883ed778f8f08b43994cb7cce
Ajay Edupuganti <ajsmac@porsche.local>
2023-12-24 18:37:20 +0530

    week 1 assigment

---
+++ assignments-master/week-1/offline-class-1/level-2/01-Class.js
@@ -0,0 +1,13 @@
+
+class Animal {
+  constructor(name, legCount) {
+    this.name = name
+    this.legCount = legCount
+  }
+  describe() {
+    return `${this.name} has ${this.legCount} legs`
+  }
+}
+
+ani = new Animal('dog','3');
+console.log(ani.describe())
